db9c21b085936fca913beb6a2212822cee620d17
Irving Henriquez <henriquezirv@gmail.com>
2021-05-05 17:25:45 +0000

    added move command to client.js

--- client.js
+++ client.js
@@ -5,17 +5,20 @@ const connect = function () {
 
     port: 50542,
   });
-  conn.on('connect',(msg)=>{
-    console.log('Successfully connected to game server')
-    conn.write('Name: IRV')
-  })
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: IRV");
+  });
+  conn.on("connect", () => {
+    // conn.write("Move: up");
+  });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-  conn.on('data',(data)=>{
-    console.log(data)
-  })
+  conn.on("data", (data) => {
+    console.log(data);
+  });
   return conn;
 };
 
-module.exports = {connect}
+module.exports = { connect };
